cd327b240d6a4ae267dcdf27e668ee28d4068286
Munkhgerelt <munkhgerelt2005@gmail.com>
2025-05-09 12:47:59 +0800

    Add multiplication function and tests with 100% coverage

---
+++ src/test/java/lab15/sict/must/edu/mn/MultiplicationTest.java
@@ -0,0 +1,18 @@
+package lab15.sict.must.edu.mn;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import org.junit.jupiter.api.Test;
+
+public class MultiplicationTest {
+    @Test
+    void testMultiplyPositive() {
+        Multiplication calc = new Multiplication();
+        assertEquals(6.0, calc.multiply(2.0, 3.0), "2 * 3 should equal 6");
+    }
+
+    @Test
+    void testMultiplyNegative() {
+        Multiplication calc = new Multiplication();
+        assertEquals(-6.0, calc.multiply(-2.0, 3.0), "-2 * 3 should equal -6");
+    }
+}
